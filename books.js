550c5410a75a96b2fa761f6a9ecd27fe0adafbca
Dotan <dotan.hillel@gmail.com>
2025-12-26 15:24:42 +0200

    Add files via upload

---
+++ books.js
@@ -0,0 +1,26 @@
+
+const myBooks = [
+    {
+        title: "The Hitchhiker's Guide",
+        author: "Douglas Adams",
+		paws: "🐾🐾🐾🐾",
+        image: "images/books/galaxy.jpg",
+        description: "A hilarious space journey...",
+        link: "book-page.html",
+		status: "popular",
+		tags: ["glaxy", "action"],
+		summary: "Arthur Dent, whose house is about to be demolished for a planned road bypass, is lying down in front of a bulldozer when his friend Ford Prefect arrives and tells him that it is imperative that they go to the pub immediately."
+	},
+    {
+        series: "Percy Jackson",
+		title: "Lightning Thief",
+        author: "Rick Riordan",
+		paws: "🐾🐾🐾🐾🐾",
+        image: "pictures/books/percy-jakson/lightning-thief.JPG",
+        description: "A boy discovers he's a demigod...",
+        link: "percy.html",
+		status: "new",
+		tags: ["Mithology", "Gods", "Monsters", "Teenagers"],
+		summary: "A boy discovers he's a demigod and starts a journey to prevent a war between Greek gods. A masterpiece of modern fantasy!"
+    }
+];
